88bc81c6d8797c78130b13419490f3aa6b9c33eb
Szalai Szabolcs <sz.szalai16@gmail.com>
2021-11-01 17:06:39 +0100

    Szamla osztály

---
+++ src/hu/petrik/bankiszolgaltatasok/Szamla.java
@@ -0,0 +1,21 @@
+package hu.petrik.bankiszolgaltatasok;
+
+public class Szamla extends BankiSzolgaltatas{
+    protected int aktualisEgyenleg;
+
+    public Szamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+    }
+
+    public int getAktualisEgyenleg() {
+        return aktualisEgyenleg;
+    }
+
+    public void befizet(int osszeg) {
+        this.aktualisEgyenleg += osszeg;
+    }
+
+    public boolean kivesz(int osszeg) {
+        return false;
+    }
+}
